5d3e5989943c71a3b9a21c4a38d1bc7b1bd4541c
MrYuriy <yuriybabiyk@gmail.com>
2023-11-27 19:55:50 +0100

    Use new black code style

--- langy/wsgi.py
+++ langy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'langy.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "langy.settings")
 
 application = get_wsgi_application()
