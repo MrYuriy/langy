85f06cb6002c33f1488e353a71ca4f240bf15419
MrYuriy <yuriybabiyk@gmail.com>
2023-11-27 22:02:23 +0100

    Add swagger

--- langy/settings.py
+++ langy/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
