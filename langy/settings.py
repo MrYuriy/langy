dac0641bed85ae12e9b8e5c6d67dbc4c78087dda
MrYuriy <yuriybabiyk@gmail.com>
2023-11-27 21:38:44 +0100

    Add contenet of static folder

--- langy/settings.py
+++ langy/settings.py
@@ -153,7 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "static/"
+
 STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = "staticfiles/"
+ASSETS_ROOT = "/static/assets"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
