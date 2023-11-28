2f5d842cfbafc065c8f2084c574def3c3099c2d2
MrYuriy <yuriybabiyk@gmail.com>
2023-11-28 17:02:33 +0100

    Refresh endpoint for more logicaly

--- user/urls.py
+++ user/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from user import views
 
 urlpatterns = [
-    path("", views.UserRegistrationAPIView.as_view(), name="create-user"),
-    path("token/", views.UserLoginAPIView.as_view(), name="login-user"),
+    path("register/", views.UserRegistrationAPIView.as_view(), name="create-user"),
+    path("login/", views.UserLoginAPIView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("logout/", views.UserLogoutAPIView.as_view(), name="logout-user"),
     path("me/", views.UserAPIView.as_view(), name="user-info"),
