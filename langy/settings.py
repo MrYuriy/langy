0b0f8300e04bd84ada976eb2067c6cc8aeb6a2ba
MrYuriy <yuriybabiyk@gmail.com>
2023-11-30 15:26:38 +0100

    Add exstra CORS

--- langy/settings.py
+++ langy/settings.py
@@ -170,4 +170,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
+    "https://localhost:5173",
+    "https://127.0.0.1:5173",
+    "https://localhost:3000",
 ]
