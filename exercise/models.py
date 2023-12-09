55e3d72af72371022f18092d5c7d89701a4c0cf1
MrYuriy <yuriybabiyk@gmail.com>
2023-12-09 18:41:23 +0100

    Add Language Word UserWord models

---
+++ exercise/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from langy import settings
+from datetime import date
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Word(models.Model):
+    DIFFICULT_LEVEL_CHOICES = (
+        ("Easy", "Eazy"),
+        ("Middle", "Middle"),
+        ("Hard", "Hard"),
+    )
+
+    name = models.CharField(max_length=20)
+    language = models.ManyToManyField(Language)
+    difficulty_level = models.CharField(max_length=6, choices=DIFFICULT_LEVEL_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class UserWord(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    native_word = models.ForeignKey(Word, on_delete=models.CASCADE, related_name="native_word")
+    day_to_repeat = models.DateField(auto_now=date.today(), blank=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.word.name
---
+++ exercise/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from exercise import views
+
+urlpatterns = [
+]
+
+app_name = "exercise"
